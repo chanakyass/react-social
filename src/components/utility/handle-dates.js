5458822368349be7dc8732ae7eac6f6dee7e23cb
Chanakya <sunkarapallychanakya@gmail.com>
2021-04-15 09:15:16 +0530

    utc date bug fixed

--- src/components/utility/handle-dates.js
+++ src/components/utility/handle-dates.js
@@ -1,20 +1,27 @@
 import moment from 'moment';
 
 export const convertDateToReadableFormat = (itemUTCString) => {
-  var itemUTC = moment.utc(itemUTCString);
+  if (itemUTCString) {
+    if (!itemUTCString.match(/Z$/gi)) {
+      console.log(itemUTCString.match(/$Z/gi));
+      itemUTCString += 'Z';
+    }
+    var itemUTC = moment.utc(itemUTCString);
 
-  var currentUTC = moment.utc();
+    var currentUTC = moment.utc();
 
-  let days = Math.abs(itemUTC.diff(currentUTC, "days"));
-  let months = Math.abs(itemUTC.diff(currentUTC, "months"));
+    let days = Math.abs(itemUTC.diff(currentUTC, "days"));
+    let months = Math.abs(itemUTC.diff(currentUTC, "months"));
 
-  if (days < 1) {
-    return itemUTC.from(currentUTC);
-  } else if (days >= 1 && days < 7) {
-    return moment(itemUTCString).local().format("dddd HH:mm A");
-  } else if (months < 12) {
-    return moment(itemUTCString).local().format("dddd, MMM Do");
+    if (days < 1) {
+      return itemUTC.from(currentUTC);
+    } else if (days >= 1 && days < 7) {
+      return moment(itemUTCString).local().format("dddd HH:mm A");
+    } else if (months < 12) {
+      return moment(itemUTCString).local().format("dddd, MMM Do");
+    }
+
+    return moment(itemUTCString).local().format("dddd, MMM Do YYYY");
   }
-
-  return moment(itemUTCString).local().format("dddd, MMM Do YYYY");
+  return "Not provided";
 };
